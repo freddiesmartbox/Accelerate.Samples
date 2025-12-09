6ee91e6097a19afc619079049b084d8d44bd1e72
Freddie Nash <Freddie.Nash@thinksmartbox.com>
2025-12-09 10:19:34 +0000

    Fudges pre ticket 1083

--- MediaPlayerSample/Avalonia.Media.MultiViewDemo/ViewModels/SimpleViewModel.cs
+++ MediaPlayerSample/Avalonia.Media.MultiViewDemo/ViewModels/SimpleViewModel.cs
@@ -11,6 +11,7 @@ namespace Avalonia.Media.MultiViewDemo.ViewModels
         [ObservableProperty] private TimeSpan? _position;
         [ObservableProperty] private long _ticks = 1;
         [ObservableProperty] private long _currentPosition;
+        [ObservableProperty] private bool _autoplay = true;
         private bool _initialized;
 
         public MediaPlayer Player { get; } = new MediaPlayer();
@@ -32,6 +33,11 @@ namespace Avalonia.Media.MultiViewDemo.ViewModels
                     await Player.SetSourceAsync(Source);
                     await Player.PrepareAsync();
                 }
+            }
+
+            if (e.PropertyName == nameof(Autoplay))
+            {
+                Player.LoadedBehavior = Autoplay ? MediaPlayerLoadedBehavior.AutoPlay : MediaPlayerLoadedBehavior.Manual;
             }
         }
 
