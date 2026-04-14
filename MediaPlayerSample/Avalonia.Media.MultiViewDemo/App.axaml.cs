148b221451902d6a11325f88ad0eb0760ade71da
Freddie Nash <Freddie.Nash@thinksmartbox.com>
2026-04-14 14:37:35 +0100

    Rough port to Avaloina 12
    Unloaded iOS and Android projects so they may be broken

--- MediaPlayerSample/Avalonia.Media.MultiViewDemo/App.axaml.cs
+++ MediaPlayerSample/Avalonia.Media.MultiViewDemo/App.axaml.cs
@@ -21,9 +21,6 @@ public partial class App : Application
     {
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
-            // Avoid duplicate validations from both Avalonia and the CommunityToolkit. 
-            // More info: https://docs.avaloniaui.net/docs/guides/development-guides/data-validation#manage-validationplugins
-            DisableAvaloniaDataAnnotationValidation();
             desktop.MainWindow = new MainWindow
             {
                 DataContext = new SimplePlayer(),
@@ -35,19 +32,5 @@ public partial class App : Application
         }
 
         base.OnFrameworkInitializationCompleted();
-    }
-
-    [UnconditionalSuppressMessage("Trimming", "IL2026:Members annotated with 'RequiresUnreferencedCodeAttribute' require dynamic access otherwise can break functionality when trimming application code", Justification = "<Pending>")]
-    private void DisableAvaloniaDataAnnotationValidation()
-    {
-        // Get an array of plugins to remove
-        var dataValidationPluginsToRemove =
-            BindingPlugins.DataValidators.OfType<DataAnnotationsValidationPlugin>().ToArray();
-
-        // remove each entry found
-        foreach (var plugin in dataValidationPluginsToRemove)
-        {
-            BindingPlugins.DataValidators.Remove(plugin);
-        }
     }
 }
--- MediaPlayerSample/Avalonia.Media.MultiViewDemo/ViewModels/SimpleViewModel.cs
+++ MediaPlayerSample/Avalonia.Media.MultiViewDemo/ViewModels/SimpleViewModel.cs
@@ -26,8 +26,11 @@ namespace Avalonia.Media.MultiViewDemo.ViewModels
             if (e.PropertyName == nameof(Source))
             {
                 await Player.StopAsync();
-                Player.Source = Source;
-                await Player.PrepareAsync();
+                if (Source is not null)
+                {
+                    await Player.SetSourceAsync(Source);
+                    await Player.PrepareAsync();
+                }
             }
         }
 
